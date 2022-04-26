0c6f326520d5da0a21319173e3c4a47cb4281c9e
M.Dotta <mamaaartin@gmail.com>
2022-04-26 03:19:26 -0300

    primer desafio

--- script.js
+++ script.js
@@ -10,16 +10,15 @@ do {
                 millas = parseFloat(prompt('iNSERTE UNA CANTIDAD DE MILLAS:'))
                 if (millas <= 0 || isNaN(millas)) { alert('millas debe ser un número mayor a cero.') }
             } while (millas < 0 || isNaN(millas))
-            kilometros = millas * 1.609
-            alert("RESULTADO: " + millas + " MILLAS SON " + kilometros + " KILOMETROS")
+            millastokilometros(millas, kilometros)
+            alert("RESULTADO: " + millas + " MILLAS SON " + millastokilometros(millas) + " KILOMETROS")
             break
         case 2:
             do {
                 kilometros = parseFloat(prompt('iNSERTE UNA CANTIDAD DE KILOMETROS:'))
                 if (kilometros <= 0 || isNaN(kilometros)) { alert('kilometros debe ser un número mayor a cero.') }
             } while (kilometros < 0 || isNaN(kilometros))
-            millas = kilometros / 1.609
-            alert("RESULTADO: " + kilometros + " KILOMETROS SON " + millas + " MILLAS")
+            alert("RESULTADO: " + kilometros + " KILOMETROS SON " + kilometrostomillas(kilometros) + " MILLAS")
             break
         case 3:
             alert("ADIOS!")
@@ -28,3 +27,13 @@ do {
             alert("OPCION INCORRECTA")
     }
 } while (mk !== 3)
+
+function millastokilometros(millas) {
+    kilometros = millas * 1.609
+    return kilometros
+}
+function kilometrostomillas(kilometros) {
+    millas = kilometros / 1.609
+    return millas
+
+}
